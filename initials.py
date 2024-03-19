b87508231c9bada3e18fefc1f9951dab6d1615ba
Livia Branco <liviadbranco@gmail.com>
2024-03-19 18:58:50 +0000

    Adds initials.py

---
+++ initials.py
@@ -0,0 +1,8 @@
+# Hi, i'm Livia and my favorite books are the ones of the Game of Thrones saga. 
+print('L             B B B B')
+print('L             B       B')
+print('L             B       B')
+print('L             B B B B')
+print('L             B       B')
+print('L             B       B')
+print('L L L L L     B B B B')
