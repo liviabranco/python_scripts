b5b3234b43f53587affe9276e5b6eed6239503de
Livia Branco <liviadbranco@gmail.com>
2024-03-19 19:05:58 +0000

    Adds letter.py

---
+++ letter.py
@@ -0,0 +1,12 @@
+#Use print() to output:
+
+#Today's date.
+#How you are feeling right now.
+#What you want to accomplish by learning to code.
+#A little message to your older, wiser, and programmer self.
+#Your favorite emoji to spice things up!
+
+print('19/03/2024')
+print('Ambicious')
+print("Hey, I hope you are happy!")
+print('💖')
